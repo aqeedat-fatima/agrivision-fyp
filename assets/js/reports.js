1496769e6b399c137102e43dbefc6d752fc185dd
aqeedat-fatima <aqeedat57@gmail.com>
2026-05-01 19:23:39 +0500

    Changed API URL

--- assets/js/reports.js
+++ assets/js/reports.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ===============================
   // 🔐 AUTH HELPERS
   // ===============================
-  const API_BASE = "";
+  const API_BASE = "http://51.21.180.202:8001";
 
   function safeJSON(v, fallback) {
     try {
